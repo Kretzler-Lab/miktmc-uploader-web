ef20e6aa6501c7068b1aea94ec4b14eb30e276cc
Becky Reamy <rlreamy@umich.edu>
2024-05-13 11:46:16 -0400

    IP-434: try to get default value to work

--- src/components/Upload/Forms/FormComponents/TextField.js
+++ src/components/Upload/Forms/FormComponents/TextField.js
@@ -32,6 +32,7 @@ class TextField extends Component {
 		if (isDisabled) {
 			this.clearContents();
 		}
+		console.log(this.props.defaultValue);
 
 		return (
 			<Form.Item label={this.props.label} validateStatus={error ? 'error' : ''}>
