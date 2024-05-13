424b54ef5bd520829dce5c733216bddc0ffa6b99
Becky Reamy <rlreamy@umich.edu>
2024-05-13 12:00:35 -0400

    IP-434: try to get default value to work

--- src/components/Upload/Forms/FormComponents/TextField.js
+++ src/components/Upload/Forms/FormComponents/TextField.js
@@ -36,7 +36,7 @@ class TextField extends Component {
 		return (
 			<Form.Item label={this.props.label} validateStatus={error ? 'error' : ''}>
 				{getFieldDecorator(this.props.fieldName, fieldOptions)(
-					<Input name={this.props.fieldName} disabled={isDisabled} placeholder={placeholderText} defaultValue="mysite"/>
+					<Input name={this.props.fieldName} disabled={isDisabled} placeholder={placeholderText}>{this.props.defaultValue}</Input>
 				)}
 			</Form.Item>
 		);
