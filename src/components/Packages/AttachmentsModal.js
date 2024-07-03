78e2a6d3a7105fd544d4ad5b821727023e19e32c
dert1129 <nathancreger1111@gmail.com>
2024-07-03 11:02:20 -0400

    add classname and z-index

--- src/components/Packages/AttachmentsModal.js
+++ src/components/Packages/AttachmentsModal.js
@@ -37,7 +37,7 @@ class AttachmentsModal extends Component {
                     {
                         (
                             this.state.showPopover &&
-                            <UncontrolledPopover flip placement='bottom' target={"attachment-popover-" + index} trigger="legacy">
+                            <UncontrolledPopover flip placement='bottom' target={"attachment-popover-" + index} trigger="legacy" className='popover-attachment'>
                                 <PopoverBody>
                                     <p className='confirmPopoverText'><b>Are you sure?</b></p>
                                     <FontAwesomeIcon icon={faSquareXmark} onClick={this.showHidePopover} className='text-danger xMark clickable' />
