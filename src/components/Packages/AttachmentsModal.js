7922ddb2b2fe2d5b6fe232bc0ab70707efe67b7e
Zach Wright <zwright@umich.edu>
2024-07-24 15:46:35 -0400

    IP-479: wait to close FU

--- src/components/Packages/AttachmentsModal.js
+++ src/components/Packages/AttachmentsModal.js
@@ -73,7 +73,7 @@ class AttachmentsModal extends Component {
     }
 
     async handleUpload() {
-       this.props.uploadFiles(this.props.packageId, uploader)
+       await this.props.uploadFiles(this.props.packageId, uploader);
         await clearCache();
        this.setState({ showFineUploader: false })
     }
