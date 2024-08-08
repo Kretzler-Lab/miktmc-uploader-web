c1e6635a9662d84bb39c798e2ab406fe15bdde80
Zach Wright <zwright@umich.edu>
2024-08-08 15:55:13 -0400

    IP-481: catch dupes

--- src/actions/Packages/packageActions.js
+++ src/actions/Packages/packageActions.js
@@ -183,27 +183,32 @@ export const replaceFile = (packageId, fileId, uploader) => {
 		dispatch(setIsUploading(true));
 		api.post('/api/v1/packages/' + packageId + '/files/replace/'+ fileId, packageInfo, {params: {hostname: window.location.hostname}})
 			.then(res => {
-				let returnedFiles = res.data;
-				uploader.on('allComplete', function (succeeded, failed) {
-					if (succeeded.length === totalFiles) {
-						dispatch(finishPackage(packageId, false));
-					} else if (succeeded.length > 0 && failed.length > 0) {
-						dispatch(finishPackage(packageId, false));
-						dispatch(alert("We were unable to upload all of your files. Check for duplicate files."));
-						dispatch(sendMessageToBackend("Unable to upload all files in package.", "Total files: " + totalFiles + " succeeded: " + succeeded.length));
-					} else if (failed.length === totalFiles) {
-						dispatch(alert("We were unable to upload any of your files. Check for duplicate files or contact support if problem persists."));
-						dispatch(sendMessageToBackend("Unable to upload all files in package.", "Total files: " + totalFiles + " succeeded: " + succeeded.length));
-					}
-				});
-				uploader.methods.setEndpoint(api.fixArguments(['/api/v1/packages/' + packageId + '/files']));
-				uploader.methods.uploadStoredFiles();
+				if (res.data.success) {
+					uploader.on('allComplete', function (succeeded, failed) {
+						if (succeeded.length === totalFiles) {
+							dispatch(finishPackage(packageId, false));
+						} else if (succeeded.length > 0 && failed.length > 0) {
+							dispatch(finishPackage(packageId, false));
+							dispatch(alert("We were unable to upload all of your files. Check for duplicate files."));
+							dispatch(sendMessageToBackend("Unable to upload all files in package.", "Total files: " + totalFiles + " succeeded: " + succeeded.length));
+						} else if (failed.length === totalFiles) {
+							dispatch(alert("We were unable to upload any of your files. Check for duplicate files or contact support if problem persists."));
+							dispatch(sendMessageToBackend("Unable to upload all files in package.", "Total files: " + totalFiles + " succeeded: " + succeeded.length));
+						}
+					});
+					uploader.methods.setEndpoint(api.fixArguments(['/api/v1/packages/' + packageId + '/files']));
+					uploader.methods.uploadStoredFiles();
+				} else {
+					dispatch(alert("We were unable to upload your file. Is it a duplicate?"));
+					dispatch(setIsUploading(false));
+				}
+
 			})
 			.catch(err => {
 				console.log(err)
 				dispatch(sendMessageToBackend(err));
 				dispatch(setIsUploading(false));
-			});;
+			});
 	}
 }
 
